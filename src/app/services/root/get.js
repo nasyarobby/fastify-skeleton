f187f1aeb33d43b10fd097447f983ec7bfa379d4
NSRB <nasyarobby@gmail.com>
2021-03-27 10:07:12 +0700

    using xsend

--- src/app/services/root/get.js
+++ src/app/services/root/get.js
@@ -1,3 +1,4 @@
 module.exports = function getRoot(req, res) {
-  res.box(`Edit this file at ${__filename}`, { data: 'Sample Data' });
+  // dapat juga menggunakan res.box
+  res.xsend(`Edit this file at ${__filename}`, { data: 'Sample Data' });
 };
