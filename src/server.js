05f23dce41cf22cff717c1a10a6fcec5d2b3c8e4
NSRB <nasyarobby@gmail.com>
2021-04-24 14:02:28 +0700

    add presets

--- src/server.js
+++ src/server.js
@@ -3,7 +3,13 @@ const DJPApi = require('djp-api');
 const port = process.env.PORT || 3000;
 const address = process.env.LISTEN_ADDRESS || '0.0.0.0';
 const specificationFilePath = `${__dirname}/app/swagger.json`;
-const service = require('./app/services');
+const service = require('./app/controllers');
 
-const app = new DJPApi({ port, address, specificationFilePath, service });
-app.start();
+const Server = new DJPApi({
+  port,
+  address,
+  specificationFilePath,
+  service,
+  logger: DJPApi.PRESETS.logger[process.env.NODE_ENV || 'development'],
+});
+Server.start();
